2db9105153da6185b589f0b72a52c26c117d6f0b
Fernanda Tovar <tovfer17@gmail.com>
2022-02-09 17:21:38 -0500

    fifth

--- PracticeProgramming/Foundations/src/main/java/scanner/DontForgetToStoreIt.java
+++ PracticeProgramming/Foundations/src/main/java/scanner/DontForgetToStoreIt.java
@@ -1,8 +1,10 @@
+package scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.sg.foundations.scanner;
+
 
 /**
  *
